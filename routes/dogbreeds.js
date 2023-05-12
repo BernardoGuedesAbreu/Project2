ef4f45deebaecbef6be7d97d0a3895672698854f
BernardoGuedesAbreu <bernardoabreu1991@gmail.com>
2023-05-12 16:48:46 +0100

    1st

---
+++ routes/dogbreeds.js
@@ -0,0 +1,11 @@
+
+//Requiring Packages
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
+
+router.get("/dogbreeds", (req,res)=>{
+    
+
+
+})
